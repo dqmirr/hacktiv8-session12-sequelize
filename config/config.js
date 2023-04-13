595ed34301e054f0afa33061c69af7e3a05e8e25
ariftoteles <arif.santoso4@bithealth.com>
2023-04-13 20:47:34 +0700

    setup for deploy production

--- config/config.js
+++ config/config.js
@@ -16,6 +16,14 @@ const config = {
     host: process.env.DB_HOST,
     dialect: "postgres",
     port: process.env.DB_PORT
+  },
+  production: {
+    username: process.env.DB_USERNAME_PROD,
+    password: process.env.DB_PASSWORD_PROD,
+    database: process.env.DB_NAME_TEST_PROD,
+    host: process.env.DB_HOST_PROD,
+    dialect: "postgres",
+    port: process.env.DB_PORT_PROD
   }
 }
 
